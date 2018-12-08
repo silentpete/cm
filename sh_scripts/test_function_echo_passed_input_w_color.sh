327a277b6c95b89ec4d7c2e0097e63518bce1dd6
Peter Gallerani <peter.gallerani@gmail.com>
2018-12-07 17:50:42 -0700

    update my default layout script

--- sh_scripts/test_function_echo_passed_input_w_color.sh
+++ sh_scripts/test_function_echo_passed_input_w_color.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
 
+# OPTIONS
+# DRYRUN
+# Running with DRYRUN will not run the commands, but print them.
+# VERBOSE
+# Running with VERBOSE will add addition output you may want for debugging or extra verbage.
+
+# info: pass in arguments and they will all print in green.
 function info () {
-  echo -e "\e[32mINFO:\e[0m $1"
+  echo -e "\e[32mINFO: $@\e[0m"
 }
 
+# warn: pass in arguments and they will all print in yellow.
 function warn () {
-  echo -e "\e[33mWARN:\e[0m $1"
+  echo -e "\e[33mWARN: $@\e[0m"
 }
 
+# error: pass in arguments and they will all print in red, then the script will exit 1.
 function error () {
-  echo -e "\e[31mERROR:\e[0m $1"
+  echo -e "\e[31mERROR: $@\e[0m"
+  exit 1
 }
 
-info "information"
-warn "warning"
-error "error"
+# Example Use
+[[ $VERBOSE ]] && echo 'use the info function to print out something in green'
+${DRYRUN:+echo} info "information"
+[[ $VERBOSE ]] && echo 'use the warn function to print out something in yellow'
+${DRYRUN:+echo} warn "warning"
+[[ $VERBOSE ]] && echo 'use the error function to print out something in red, and exit 1'
+${DRYRUN:+echo} error "error"
 
