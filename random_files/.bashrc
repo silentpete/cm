3bcd0e45f3c0ec34a966960369aa0883f60aa760
Peter Gallerani <peter.gallerani@gmail.com>
2019-04-07 14:10:28 -0600

    update bashrc, install_go and default function colors

--- random_files/.bashrc
+++ random_files/.bashrc
@@ -1,8 +1,8 @@
-#TODO: get some colors
+# My bashrc stuff
 eval $(dircolors -b)
 export GREP_COLOR='01;32'
 PROMPT_COMMAND='_PROMPT_EXIT_STATUS=$?; _PWD_RELATIVE="${PWD/#$HOME/~}"; printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/~}"'
-PS1="\$([[ \$UID -eq 0 ]] && echo \[\033[01\;31m\] || echo \[\033[01\;32m\])\u\[\033[01;30m\]@\[\033[01;34m\]\h\[\033[01;30m\](\$([[ \${#_PWD_RELATIVE} -gt 30 ]] && echo \[\033[01\;36m\]\${_PWD_RELATIVE:0:5}\[\033[01\;30m\]...\[\033[01\;36m\]\${_PWD_RELATIVE:\${#_PWD_RELATIVE}-22} || echo \[\033[01\;36m\]\$_PWD_RELATIVE)\[\033[01;30m\]) \$([[ \$_PROMPT_EXIT_STATUS -ne 0 ]] && echo \[\033[01\;31m\] || echo \[\033[01\;32m\])\\$\[\033[00m\] "
+PS1="\$([[ \$UID -eq 0 ]] && echo \[\033[01\;31m\] || echo \[\033[01\;32m\])\u\[\033[01;33m\]@\[\033[01;34m\]\h\[\033[01;33m\](\$([[ \${#_PWD_RELATIVE} -gt 30 ]] && echo \[\033[01\;36m\]\${_PWD_RELATIVE:0:7}\[\033[01\;33m\]...\[\033[01\;36m\]\${_PWD_RELATIVE:\${#_PWD_RELATIVE}-20} || echo \[\033[01\;36m\]\$_PWD_RELATIVE)\[\033[01;33m\]) \$([[ \$_PROMPT_EXIT_STATUS -ne 0 ]] && echo \[\033[01\;31m\] || echo \[\033[01\;32m\])\\$\[\033[00m\] "
 alias grep='grep --color=auto'
 alias ls='ls --color=auto'
 alias ll='ls -al'
--- sh_scripts/install_go.sh
+++ sh_scripts/install_go.sh
@@ -1,65 +1,91 @@
 #!/bin/bash
 
+# VERBOSE
+# Running with VERBOSE will add addition output you may want for debugging or extra verbage.
+
+# debug: pass in arguments and they will all print in green.
+function debug () {
+  echo -e "\e[36mtime=\"$(date --rfc-3339='seconds')\" level=DEBUG msg=\"$@\"\e[0m"
+}
+
+# info: pass in arguments and they will all print in green.
+function info () {
+  echo -e "\e[32mtime=\"$(date --rfc-3339='seconds')\" level=INFO msg=\"$@\"\e[0m"
+}
+
+# warn: pass in arguments and they will all print in yellow.
+function warn () {
+  echo -e "\e[33mtime=\"$(date --rfc-3339='seconds')\" level=WARN msg=\"$@\"\e[0m"
+}
+
+# error: pass in arguments and they will all print in red, then the script will exit 1.
+function error () {
+  echo -e "\e[31mtime=\"$(date --rfc-3339='seconds')\" level=ERROR msg=\"$@\"\e[0m"
+  exit 1
+}
+
 default_gopath="$HOME/go"
 goVersion="1.12"
 goDir="go${goVersion}"
 
 # Set working directory
-echo 'moving to home directory'
+[[ $VERBOSE ]] && debug 'moving to home directory'
 cd
 CWD="${HOME}"
 
-# make ~/go directory
 if [[ -d ${default_gopath} ]]; then
-  echo -e "found \"${default_gopath}\""
+  [[ $VERBOSE ]] && debug "found \"${default_gopath}\""
 else
-  echo -e "did not find directory: \"${default_gopath}\", making directory"
+  info "Setup my Go coding directory"
+  [[ $VERBOSE ]] && debug "did not find directory: \"${default_gopath}\", making directory"
   mkdir "${default_gopath}"
   mkdir "${default_gopath}/bin"
   mkdir "${default_gopath}/pkg"
   mkdir "${default_gopath}/src"
+  mkdir "${default_gopath}/src/github.com"
+  mkdir "${default_gopath}/src/github.com/silentpete"
 fi
 
-# make ~/go<ver> directory
 if [[ -d ${goDir} ]]; then
-  echo -e "found \"${HOME}/${goDir}\""
+  [[ $VERBOSE ]] && debug "found \"${HOME}/${goDir}\""
 else
-  echo -e "did not find directory: \"${HOME}/${goDir}\", making directory"
+  info "Create ~/go<ver> directory"
+  [[ $VERBOSE ]] && debug "did not find directory: \"${HOME}/${goDir}\", making directory"
   mkdir "${goDir}"
 
-  # Set go directory full path
+  info "Set go directory full path"
   goInstallPath="${CWD}/${goDir}"
 
-  echo 'downloading go'
+  info 'downloading Go tar.gz'
   curl --insecure --progress-bar "https://dl.google.com/go/${goDir}.linux-amd64.tar.gz" | tar -xz --directory "${goInstallPath}" --strip-components=1
 fi
 
 path_adjusted="false"
 # TODO: add better checking
 if [[ -z ${GOROOT+x} ]]; then
-  echo 'GOROOT not set'
-  echo 'setting GOROOT'
+  info "Set GOROOT"
+  [[ $VERBOSE ]] && debug 'GOROOT not set, setting GOROOT'
   echo "export GOROOT=\$HOME/${goDir}" >> ~/.bashrc
-  echo 'add GOROOT to path'
-  echo 'PATH=$PATH:$GOROOT/bin' >> ~/.bashrc
+  [[ $VERBOSE ]] && debug 'add GOROOT to path'
+  echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.bashrc
   path_adjusted="true"
 else
-  echo 'variable GOROOT already set'
+  [[ $VERBOSE ]] && debug 'variable GOROOT already set'
 fi
 
 if [[ -z ${GOPATH+x} ]]; then
-  echo 'GOPATH not set'
-  echo 'setting GOPATH'
+  info "Set GOPATH"
+  [[ $VERBOSE ]] && debug 'GOPATH not set, setting GOPATH'
   echo "export GOPATH=${default_gopath}" >> ~/.bashrc
-  echo 'add GOPATH to path'
-  echo 'PATH=$PATH:$GOPATH' >> ~/.bashrc
+  [[ $VERBOSE ]] && debug 'add GOPATH to path'
+  echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
   path_adjusted="true"
 else
-  echo 'variable GOPATH already set'
+  [[ $VERBOSE ]] && debug 'variable GOPATH already set'
 fi
 
 if [[ "${path_adjusted}" == "true" ]]; then
-  echo 'Please logout and back in to reset your env, or run the following: . ~/.bashrc'
+  info 'Please logout and back in to reset your env, or run the following: . ~/.bashrc'
 fi
 
 # check if go exist should produce...
